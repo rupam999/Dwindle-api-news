b6684744060ed4d0ba258f2574ab846eb074b3eb
rupam chakraborty <rupamchakraborty999@gmail.com>
2020-03-10 22:28:28 +0530

    python interation

--- index.js
+++ index.js
@@ -268,11 +268,8 @@ app.get("/currency/:originCurrency", function(req, res){
     });
 });
 
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-}
-app.get('*',(req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+app.get("*", function(req, res){
+    res.send('<h1></h1>ERROR 404 NOT FOUND</h1>');
 });
 
 let PORT = process.env.PORT || 3000
